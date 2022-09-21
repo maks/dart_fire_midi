63329a3e34261a095c0ff5e9d5dd0b5770d819a0
Maksim Lin <maks@manichord.com>
2022-09-21 10:38:54 +1000

    add button led on/off helper

--- lib/dart_fire_midi.dart
+++ lib/dart_fire_midi.dart
@@ -1,4 +1,5 @@
 library dart_fire_midi;
 
 export 'src/fire_device.dart';
+export 'src/buttons.dart';
 export 'src/input_events.dart';
---
+++ lib/src/buttons.dart
@@ -0,0 +1,31 @@
+import 'dart:typed_data';
+
+import 'cc_inputs.dart';
+
+enum ButtonCode {
+  browser(CC.browser),
+  patternUp(CC.patternUp),
+  patternDown(CC.patternDown),
+  gridLeft(CC.gridLeft),
+  gridRight(CC.gridRight),
+  step(CC.step),
+  note(CC.note),
+  drum(CC.drum),
+  perform(CC.perform),
+  shift(CC.shift),
+  alt(CC.alt),
+  pattern(CC.pattern),
+  soloMute1(CC.muteButton1),
+  soloMute2(CC.muteButton2),
+  soloMute3(CC.muteButton3),
+  soloMute4(CC.muteButton4);
+
+  const ButtonCode(this.code);
+  final int code;
+}
+
+class ButtonControls {
+  static Uint8List buttonOn(ButtonCode button, int ledColour) {
+    return CC.on(button.code, ledColour);
+  }
+}
